073f4e2ca97f1e402b0974a2dc3362f2cc141484
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-02-28 23:42:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if !(board[index].between?(0, 8))
-  return true
+  if !(board[index].between?(0, 8)) 
+  return false
   end
 end
 
